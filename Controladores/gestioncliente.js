e1489a2b663d0c82a1dd20370a879b550f0d2843
GabrielJEG95 <gabrieljeg95@gmail.com>
2022-02-09 11:10:55 -0600

    Add files via upload

--- Controladores/gestioncliente.js
+++ Controladores/gestioncliente.js
@@ -1,16 +1,22 @@
 $(document).ready(function(){
   $('#cmbGestion').load('Controladores/obtenerGestionReal.php');
   obtenerGestiones();
+  obtenerHistGestiones()
 });
 
 $('#btnActualizar').click(()=>{
   obtenerGestiones();
 })
 
+$('#btnActualizarGestiones').click(()=>{
+  obtenerHistGestiones();
+})
+
 $(document).on("change","#chkSelect",function(e){
   fila=$(this).closest("tr");
   id= fila.find('td:eq(1)').text();
   gestion=fila.find('td:eq(2)').text()
+  $('#txtIdGestion').val(id);
   if (this.checked) {
       $('#strGestion').text(id + " "+ gestion)
   }else{
@@ -21,6 +27,22 @@ $(document).on("change","#chkSelect",function(e){
 
 $('#btnSave').click(()=>{
   var data = new FormData($('#formGestiones')[0]);
+  var nombre=$('#txtNombre').val();
+  var apellido=$('#txtApellido').val();
+  var direccion=$('#txtDireccion').val();
+  var solucion=$('#txtSolucion').val();
+  var problema=$('#txtProblema').val();
+  var gestion=$('#cmbGestion').val();
+  var phone=$('#txtPhone').val();
+  var idgestion=$('#txtIdGestion').val();
+  data.append('txtNombre',nombre);
+  data.append('txtApellido',apellido);
+  data.append('txtDireccion',direccion);
+  data.append('txtSolucion',solucion);
+  data.append('txtProblema',problema);
+  data.append('cmbGestion',gestion);
+  data.append('txtPhone',phone);
+  data.append('txtIdGestion',idgestion);
   console.log(data)
   $.ajax({
     url: "Controladores/guardarTickets.php",
@@ -48,6 +70,7 @@ $('#btnSave').click(()=>{
     }
   });
 })
+
 function limpiarCampos(){
   $('#txtNombre').val('');
   $('#txtApellido').val('');
@@ -82,6 +105,44 @@ function obtenerGestiones(){
     "columns": [
       {"defaultContent": "<div class='text-center'><div class='btn-group'> <input id='chkSelect' type='checkbox'> </div></div>"},
       {"data": "CodGestion"},
+      {"data":"NombreGestion"}
+    ],
+    "paging": false,
+    "lengthChange": true,
+    "searching": false,
+    "ordering": true,
+    "info": false,
+    "autoWidth": true,
+    "destroy": true,
+    "language": {
+      "emptyTable": "No existen registros en la base de datos",
+      "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+      "lengthMenu": "Mostrar _MENU_ registros",
+      "infoFiltered": "(Filtrado de _MAX_ total registros)",
+      "infoEmpty": "Mostrando 0 de 0 de 0 registros",
+      "paginate": {
+        "first": "Primero",
+        "last": "Ultimo",
+        "next": "Siguiente",
+        "previous": "Anterior"
+      }
+    }
+  });
+}
+
+function obtenerHistGestiones(){
+  $('#tblHist').DataTable({
+    "bDeferRender": true,
+    "sPaginationType": "full_numbers",
+    "ajax": {
+      "url": "Controladores/tickets.php",
+      "method": 'GET', //usamos el metodo POST
+      "dataSrc": "Data"
+    },
+    "columns": [
+      {"data": "NombreCliente"},
+      {"data":"ApellidoCliente"},
+      {"data":"FechaCreacion"},
       {"data":"NombreGestion"}
     ],
     "paging": false,
